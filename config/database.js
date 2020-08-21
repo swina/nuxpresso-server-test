1e8a9182381a9e9c460b99197c7ad296ffe1cf16
Swina Allen <swina.allen@gmail.com>
2020-08-22 00:16:18 +0300

    Reset db config

--- config/database.js
+++ config/database.js
@@ -5,7 +5,7 @@ module.exports = ({ env }) => ({
       connector: 'bookshelf',
       settings: {
         client: 'postgres',
-        host: env('DATABASE_HOST'),
+        host: env('DATABASE_URL'),
         port: env.int('DATABASE_PORT'),
         database: env('DATABASE_NAME'),
         username: env('DATABASE_USERNAME'),
