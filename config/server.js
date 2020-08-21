b7fa616691498a03abd44e45358e0b0d3fc34b1e
Swina Allen <swina.allen@gmail.com>
2020-08-22 00:43:27 +0300

    Changed server config

--- config/server.js
+++ config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   host: env('HOST', process.env.HOST || '0.0.0.0'),
-  port: env.int('PORT', process.env.PORT || 1337),
+  port: env.int('PORT', process.env.PORT || 3000),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET'),
